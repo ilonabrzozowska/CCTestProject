d3f79486c93beda5c5ac2100515e5920b6fff53c
Ilona Brzozowska <C:\The Bat!\Mail>
2016-06-27 19:12:47 +0200

    Added feature that change every 'ipsum' word into red for three seconds after click 'push the button'

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
     
-    $(".top").click(function() {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
+    $('.top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
         return false;
+    });
+    
+    $('#button-push').click(function() {
+        var txt = '';
+        $('article p').each(function() {
+            txt = $(this).html().replace('ipsum', '<span class="red">ipsum</span>')
+            txt = '<p>' + txt + '</p>';
+            $(this).replaceWith(txt);
+        })     
+        setTimeout(function() {
+            $('article p').each(function() {
+                txt = $(this).html().replace('<span class="red">ipsum</span>', 'ipsum');
+                txt = '<p>' + txt + '</p>';
+                $(this).replaceWith(txt);
+            })
+        }, 3000);      
     });
     
 })
